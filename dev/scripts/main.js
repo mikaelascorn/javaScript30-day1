c89eabdce28e1af91b6bf688ebf204c895cc3716
Mikaela Scornaienchi <scornaim@gmail.com>
2018-06-25 20:36:58 -0400

    finished

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,1 +1,29 @@
-console.log("ready")
+// this will get the key code 
+function playSound(e) {
+    // console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio ) return;  // will stop it from running all together if key not valid
+    // rewind to start of element so you can play fast
+    audio.currentTime = 0;
+    audio.play();
+
+    console.log(key);
+    // this is jquery is key.addClass('playing')
+    key.classList.add('playing');
+  };
+
+  // this gives us all the keys for the timed animation, when key pressed it finds the key and removes transition
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+  // finds the transition yellow colour on the key 
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  console.log(e.propertyName);
+  // this is the key
+  this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound );
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,33 @@
-"use strict";
+'use strict';
 
-console.log("ready");
+// this will get the key code 
+function playSound(e) {
+  // console.log(e.keyCode);
+  var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  var key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+
+  if (!audio) return; // will stop it from running all together if key not valid
+  // rewind to start of element so you can play fast
+  audio.currentTime = 0;
+  audio.play();
+
+  console.log(key);
+  // this is jquery is key.addClass('playing')
+  key.classList.add('playing');
+};
+
+// this gives us all the keys for the timed animation, when key pressed it finds the key and removes transition
+var keys = document.querySelectorAll('.key');
+keys.forEach(function (key) {
+  return key.addEventListener('transitionend', removeTransition);
+});
+
+// finds the transition yellow colour on the key 
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  console.log(e.propertyName);
+  // this is the key
+  this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
